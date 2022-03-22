1e12f715485a4e0009e4dff7546cb1d9fa2367fb
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-22 19:52:04 +0530

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    splitText = text.split('');
+    //console.log(splitText);
+    reverseText = splitText.reverse();
+    //console.log(reverseText);
+    newString = reverseText.join('');
+    //console.log(newString);
+    return newString;
 };
 
 // Do not edit below this line
