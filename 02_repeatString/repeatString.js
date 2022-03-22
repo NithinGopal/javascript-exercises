a2b2c2aee3e0931ac873accbb80f6808978c0245
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-22 19:26:56 +0530

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let newString = '';
+    for (let i=1; i<=num; i++){
+        newString = word.concat('',newString);
+      }
+     return newString;
 };
 
 // Do not edit below this line
