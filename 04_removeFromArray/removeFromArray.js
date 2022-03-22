1bb2e913c8d64ee8ea2130b25187b5fdf3b7646d
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-22 23:22:05 +0530

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
-
-};
-
+    for (j=1; j<= arguments.length; j++){
+       let compArray = arguments[j];
+       let testArray = arguments[0];
+          for (i=0; i<=testArray.length; i++){
+          if (testArray[i] === compArray){
+             testArray.splice(i,1);
+             arguments[0] = testArray;
+          }
+       }
+    }
+    return arguments[0];
+ };
+ 
 // Do not edit below this line
 module.exports = removeFromArray;
