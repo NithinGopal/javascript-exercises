b822ff61d5b5a014776f33242674ae95fef616f7
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-23 15:38:26 +0530

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  cTemp = (5/9)*(f - 32);
+  if (cTemp === 0){
+     return cTemp;
+  }else {
+     cTemp = +cTemp.toFixed(1);
+  return cTemp;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  fTemp = ((9/5)* c) + 32;
+  if (fTemp === 0){
+     return fTemp;
+  }else {
+     fTemp = +fTemp.toFixed(1);
+  return fTemp;
+  }
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
