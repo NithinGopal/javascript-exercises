9a2d43d0f25cb75eae1a4e2f370abc534c9c70f3
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-23 11:36:13 +0530

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+// const sumAll = function() {
 
-};
+// };
+
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+       return 'ERROR';
+       
+    }else if (num1 > 0 || num2 > 0) {
+       if (num1 < num2){
+          sum = num2 * (num1 + ((num2-1)/2));
+          return sum;
+       }else if (num1>num2) {
+          sum = num1 * (num2 + ((num1-1)/2));
+          return sum;
+       }
+    }
+ 
+ };
 
 // Do not edit below this line
 module.exports = sumAll;
