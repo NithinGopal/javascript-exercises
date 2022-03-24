4201d5d3345e2c54b887b870b429f85b3cb90085
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-24 09:03:52 +0530

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (text) {
+    smallText = text.toLowerCase();
+    //console.log(smallText);
+    inputText = smallText.replace(/ /g, '');
+    //console.log(inputText);
+    splitText = inputText.split('');
+     //console.log(splitText);
+    for (i=0; i<=splitText.length; i++){
+       if (splitText[i] === ' ' || splitText[i] === '.' || splitText[i] === ',' || splitText[i] === '?' || splitText[i] === '!'){
+          splitText.splice(i,1);
+       }
+    }
+    //console.log(splitText);
+    splitString = splitText.join('');
+    //console.log(splitString);
+     reverseText = splitText.reverse();
+     //console.log(reverseText);
+    reverseString = reverseText.join('');
+    //console.log(reverseString);
+   if (splitString === reverseString){
+      return true;
+   }else{
+      return false;
+   }
+    
+ };
 // Do not edit below this line
 module.exports = palindromes;
