35216f66d5125d4cbcca1a50d18ccb9cffa4ec8b
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-24 16:29:48 +0530

    Do 03
    - only two tests passed

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,17 @@
-const caesar = function() {
-
-};
+const caesar = function(text,n) {
+    let code = [];
+    let newChar = [];
+    for (i=0; i<text.length; i++){
+       if (text.charAt(i) === ',' || text.charAt(i) === '!' || text.charAt(i) === '?' || text.charAt(i) === ' '){
+          newChar[i] = text.charAt(i);
+       }else {
+          code[i] = text.charCodeAt(i) + n;
+       newChar[i] = String.fromCharCode(+code[i]);
+       }
+    }
+       let newString = newChar.join('');
+       return newString;
+    };
 
 // Do not edit below this line
 module.exports = caesar;
