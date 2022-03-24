b90201d90fee0dd01bb687ca54f3a545bd20a591
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-24 15:24:47 +0530

    Do this again

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(input) {
+    return input.reduce((oldestPerson, currentPerson) => {
+      const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      return oldestAge < currentAge ? currentPerson : oldestPerson
+    })
+  };
+  
+  const getAge = function(birth, death) {
+    if (!death) {                             
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
