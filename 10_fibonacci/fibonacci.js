8b9f7be8b06d462cf6735876a55a1f6078bb3d2b
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-24 12:01:58 +0530

    Complete 10 & 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    if (num <= 0){
+       return 'OOPS';
+    }else{
+       let fib = [0,1];
+       num = +num;
+    for (i=fib.length; i<=num; i++){
+       fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num];
+    }
+ };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(list) {
+    let x = list;
+    let titles = [];
+    for (i=0; i<x.length; i++){
+       titles[i] = x[i].title;
+    }
+    return titles;
+ 
+ };
 
 // Do not edit below this line
 module.exports = getTheTitles;
